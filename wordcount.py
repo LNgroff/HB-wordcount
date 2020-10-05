329e1d6fb336e8bde2eebd983d268d3c0b1a0e18
LNgroff <LNgroff@gmail.com>
2020-10-05 17:00:52 +0000

    Finished part1

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,31 @@
 # put your code here.
+filename = open("test.txt")
+
+def wordcount(filename):
+    # poem = open(filename)
+    #file = open("twain.txt")
+
+    wordcount = {}
+    for line in filename:
+        words = line.rstrip().split(" ")
+        
+        set_words = set(words)
+        keys = list(set_words)
+
+        for word in words:
+            wordcount[word] = wordcount.get(word, 0) + 1
+    
+    for word, value in wordcount.items():
+        print("{} {}".format(word,value))
+
+
+    filename.close()
+
+ 
+    
+
+wordcount(filename)
+
+
+special_char = ["." , "," , "?", "!" , ";", ":", "'"]
+
