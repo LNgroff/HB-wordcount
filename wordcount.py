8e1284eead0dfe7ead3efc883bb9b8958d99a470
LNgroff <LNgroff@gmail.com>
2020-10-05 19:22:00 +0000

    Part2 failure

--- wordcount.py
+++ wordcount.py
@@ -1,31 +1,44 @@
 # put your code here.
 filename = open("test.txt")
 
-def wordcount(filename):
+def getkeys(filename):
     # poem = open(filename)
     #file = open("twain.txt")
+    
 
-    wordcount = {}
     for line in filename:
         words = line.rstrip().split(" ")
         
-        set_words = set(words)
-        keys = list(set_words)
-
+        list_words = []
+        #set_list = set(list_words)
+        #keys = list(set_list)
+        
         for word in words:
-            wordcount[word] = wordcount.get(word, 0) + 1
+            word1 = word.lower().strip("',.!?-#$%^&();:_")
+            list_words.append(word1)
     
-    for word, value in wordcount.items():
-        print("{} {}".format(word,value))
-
-
     filename.close()
 
- 
+    return list_words
+
+def wordcount(list_words):       
+    wordcount = {}    
+    set_list = set(list_words)
+    keys = list(set_list)
+
+    for key in keys:
+        wordcount[key] = wordcount.get(key, 0) + 1
     
+    for key, value in wordcount.items():
+        print("{} {}".format(key,value))
+
+
+
+ 
 
 wordcount(filename)
-
-
-special_char = ["." , "," , "?", "!" , ";", ":", "'"]
-
+"""
+for word in set_words:
+    if word[-1] in special_char:
+                del word[-1]
+"""
